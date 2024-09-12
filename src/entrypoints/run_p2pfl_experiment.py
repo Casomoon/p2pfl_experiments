1d4187d703020b5dd9dd57ea1f3e3dd236f102ee
Casomoon <montagcarsten@googlemail.com>
2024-09-13 00:43:10 +0200

    TorchLightningModule changes

--- src/entrypoints/run_p2pfl_experiment.py
+++ src/entrypoints/run_p2pfl_experiment.py
@@ -3,8 +3,15 @@ from p2pfl.node import Node
 from p2pfl.learning.pytorch.mnist_examples.mnistfederated_dm import MnistFederatedDM
 from p2pfl.learning.pytorch.mnist_examples.models.mlp import MLP
 from p2pfl.communication.memory.memory_communication_protocol import InMemoryCommunicationProtocol
+# singleton logger
+from p2pfl.management.logger import logger
 from ..modelling.ernie import BERTLightningModel
+from ..modelling.nli_data_load import NLIDataModule, NLIParser
+from pathlib import Path
 import signal 
+
+root = Path(__file__).resolve().parents[2]
+mnli_data_path = root/"data"/"multinli_1.0"
 
 def stop_nodes_handler(sig, frame, nodes: list[Node]):
     print(f"Received signal {sig}. Stopping all nodes...")
@@ -28,17 +35,26 @@ def ring(nodes: list[Node]):
     print("circle_merkel")
 
 def main(): 
+    logger.info("main", f"Extracting mnli data from {mnli_data_path}.")
     comm = InMemoryCommunicationProtocol
     model = MLP
-    data = MnistFederatedDM
     nr_nodes = 5
+    data_dist_weights = [0.1, 0.15, 0.15, 0.3, 0.3]
+    batch_size = 16
+    assert len(data_dist_weights) == nr_nodes
+    assert sum(data_dist_weights) == 1.0
     nodes_refs: list[Node] = []
+    nli_data = NLIParser(mnli_data_path, nr_nodes, data_dist_weights, batch_size).get_non_iid_split()
+    exit(1)
+    
+    
+    
     for i in range(nr_nodes): 
         new_node = Node(model(),
                         data, 
                         f"mlp_test_{i}", 
                         protocol = comm)
-        new_node.start(wait=False)
+        new_node.start()
         nodes_refs.append(new_node)
     signal.signal(signal.SIGINT, lambda sig, frame: stop_nodes_handler(sig,frame,nodes_refs))
     print("test_nodes_started")
