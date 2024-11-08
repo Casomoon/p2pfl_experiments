aa8926456a6a4d2417f7230634f647c20db55956
Casomoon <montagcarsten@googlemail.com>
2024-11-08 16:23:41 +0100

    reduce learners to 18

--- p2pfl_experiments/experiments/ring_20_20.sh
+++ p2pfl_experiments/experiments/ring_20_20.sh
@@ -3,14 +3,14 @@ cd ~/p2pfl_experiments/p2pfl_experiments
 pwd
 model_name="bert"
 structure="ring"
-nr_nodes=20
-nr_learners=20
+nr_nodes=18
+nr_learners=18
 epochs_per_round=1
 rounds=10
 batch_size=2
-data_dist_weights=(0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05)
+data_dist_weights=(0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05)
 
-poetry run run_bert_p2p \
+srun poetry run run_bert_p2p \
         --model_name "$model_name"\
         --structure "$structure"\
         --nr_nodes "$nr_nodes"\
