bba5251d0722d08a6dfdcb11c3d4c4655a0ed5c9
Casomoon <montagcarsten@googlemail.com>
2024-11-11 05:11:41 +0100

    uhhhm

--- p2pfl_experiments/src/entrypoints/run_visualize_results.py
+++ p2pfl_experiments/src/entrypoints/run_visualize_results.py
@@ -2,5 +2,5 @@ from ..stats.result_visualization import ResultVisualizer
 from pathlib import Path
 def main(): 
     root = Path(__file__).resolve().parents[2]
-    print(root)
+    #results_dir = 
     #ResultVisualizer()
